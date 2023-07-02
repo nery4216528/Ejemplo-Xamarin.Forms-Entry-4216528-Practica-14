e7b74340e3af7b83baea260bbb5925d35146836c
NERY DAVID MENDEZ <4216528@estudiantes.mined.edu.sv>
2023-07-02 15:11:28 -0600

    Agregar archivos de proyecto.

---
+++ Video6/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Video6
+
+{
+
+    public partial class MainPage : ContentPage
+
+    {
+
+        public MainPage()
+
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+
+            // Creamos dos variables las cuales seran las encargadas de almacenar todos los datos enviados desde el Entry //
+        {
+
+            string Pasado = e.OldTextValue;
+
+            string Nuevo = e.NewTextValue;
+
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+
+            // Esta funcion nos permite guardar en una cadena string el contenido que ingrese en el Entry//
+
+        {
+
+            string text = ((Entry)sender).Text;
+
+        }
+
+    }
+
+}
